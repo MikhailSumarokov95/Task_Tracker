28f7197c3ffba6dad1f1c88ed30e0e0e65c1ac0a
MikhailSumarokov95 <ykpon1995@mail.ru>
2023-10-04 15:27:12 +0500

    changed the table creation script

--- src/main/resources/db/changelog/sql/init.sql
+++ src/main/resources/db/changelog/sql/init.sql
@@ -4,13 +4,11 @@ CREATE TABLE IF NOT EXISTS task_group(
     name VARCHAR(64) NOT NULL
 );
 
-ALTER TABLE task_group ADD UNIQUE (name);
-
 CREATE TABLE IF NOT EXISTS task(
     id SERIAL PRIMARY KEY,
     text VARCHAR(64) NOT NULL,
     date_created DATE NOT NULL,
-    date_dead_line DATE NOT NULL,
+    date_dead_line DATE,
     task_group_id INTEGER NOT NULL REFERENCES task_group(id),
     is_completed boolean NOT NULL DEFAULT FALSE
 );
